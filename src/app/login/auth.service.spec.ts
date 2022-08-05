14505ea899cb6b8832de4f4a1b9dc4bfaf96df3a
fabioleite <fabprogrammer@gmail.com>
2022-08-05 16:24:18 -0300

    exclusão do karma e configuração do jest

--- src/app/login/auth.service.spec.ts
+++ src/app/login/auth.service.spec.ts
@@ -1,3 +1,5 @@
+import { HomeComponent } from './../pages/home/home.component';
+import { Usuario } from './../shared/models/usuario';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 
@@ -8,7 +10,9 @@ describe('AuthService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[RouterTestingModule]
+      imports: [RouterTestingModule.withRoutes(
+        [{path: 'home', component: HomeComponent}]
+      )],
     });
     service = TestBed.inject(AuthService);
   });
@@ -16,4 +20,24 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('fazerLogin() user authorized', () => {
+    let mockUser = {
+      nome: 'fabio',
+      senha: 'fabio',
+      permissao: '1',
+    } as Usuario;
+
+    service.fazerLogin(mockUser);
+    expect(service['usuarioAutenticado']).toEqual(true);
+  });
+
+  it('fazerLogin() user unauthorized', () => {
+    let mockUser = {
+      nome: 'usuarioJ81Falso',
+      senha: 'usuarioJ81Falso',
+      permissao: '0',
+    } as Usuario;
+    expect(service['usuarioAutenticado']).toEqual(false);
+  })
 });
